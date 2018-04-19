b589415caa62de00511a554b4ce86bfd7979c877
Chris Yunbin Chang <hw@yunbin.kr>
2018-04-19 23:42:43 +0900

    separate the table by segmented control label

--- ViewController.swift
+++ ViewController.swift
@@ -11,27 +11,30 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableview: UITableView!
     
-    var tasks: [Task] = []
+    @IBOutlet weak var segmtd: UISegmentedControl!
+    
+    var tasks: [Task] = [Task(title: "ABC", section: .InProgress), Task(title: "DEF", section: .Done)]
+    var showingTasks: [Task] = []
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.tasks.count
+        return self.showingTasks.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableview.dequeueReusableCell(withIdentifier: "todoCell")
         
-        cell?.textLabel?.text = self.tasks[indexPath.row].title
+        cell?.textLabel?.text = self.showingTasks[indexPath.row].title
         
         return cell!
     }
-    
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.tableview.delegate = self
         self.tableview.dataSource = self
+        
+        makeShowingTasks(sec: .ToDo)
         
     }
 
@@ -47,10 +50,56 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             let textFld = alert?.textFields![0]
             self.tasks.append(Task(title: (textFld?.text!)!, section: .ToDo))
             print(self.tasks)
+            self.makeShowingTasks(sec: self.getSectionFromSegmtd())
             self.tableview.reloadData()
         }))
         
         self.present(alert, animated: true, completion: nil)
+    }
+    
+    func getSectionFromSegmtd() -> Section{
+        var section: Section?
+        switch segmtd.selectedSegmentIndex {
+        case 0:
+            section = .ToDo
+            break
+        case 1:
+            section = .InProgress
+            break
+        case 2:
+            section = .Done
+            break
+            
+        default:
+            print("There are some error")
+        }
+        return section!
+    }
+    
+    func makeShowingTasks(sec: Section) {
+        showingTasks.removeAll()
+        for x in tasks {
+            if x.section == sec {
+                showingTasks.append(x)
+            }
+        }
+    }
+    
+    @IBAction func onClickSegmtd(_ sender: Any) {
+        switch segmtd.selectedSegmentIndex {
+        case 0:
+            makeShowingTasks(sec: .ToDo)
+            break
+        case 1:
+            makeShowingTasks(sec: .InProgress)
+            break
+        case 2:
+            makeShowingTasks(sec: .Done)
+            break
+        default:
+            print("There are some errors")
+        }
+        self.tableview.reloadData()
     }
     
     
@@ -58,9 +107,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.didReceiveMemoryWarning()
     }
     
-    
-    
-
 
 }
 
